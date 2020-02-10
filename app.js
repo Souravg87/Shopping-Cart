363d6124d9cfd5fac8cf3f4f1a2900886c6eb322
Souravg87 <46392946+Souravg87@users.noreply.github.com>
2020-02-10 16:10:23 +0530

    Adding Products Dynamically

--- app.js
+++ app.js
@@ -6,18 +6,29 @@ const cartOverlay= document.querySelector('.cart-overlay');
 const cartItems= document.querySelector('.cart-items');
 const cartTotal= document.querySelector('.cart-total');
 const cartContent= document.querySelector('.cart-content');
-const productsDom= document.querySelector('.prdocts-center');
+const productsDOM= document.querySelector('.products-center');
 
 let cart=[];
 
 class Products {
  async getProducts(){
-   try {
+   try 
+   {
      let result= await fetch('products.json');
-     return result;
+     let data = await result.json();
 
-   } catch (e) {
-     console.log(e);
+     let products=data.items;
+     products = products.map(item =>{
+       const {title,price}=item.fields;
+       const {id} = item.sys;
+       const image = item.fields.image.fields.file.url;
+       return {title,price,id,image}
+     })
+     return products;
+   } 
+   catch (error) 
+   {
+     console.log(error);
    }
  }
 
@@ -25,19 +36,42 @@ class Products {
 
 class UI {
   displayproducts(products){
-    console.log(products);
+    let result='';
+    products.forEach(product => {
+      result += `
+      <article class="product">
+        <div class="img-container">
+          <img src=${product.image} alt="product" class="product-img">
+          <button class="bag-btn" data-id=${product.id}>
+            <i class="fas fa-shopping-cart"></i>
+            add to bag
+          </button>
+        </div>
+        <h3>${product.title}</h3>
+        <h4>$${product.price}</h4>
+      </article>
+      `
+    });
+    productsDOM.innerHTML = result;
 
   }
 
 }
 
 class Storage{
-
+  static saveProducts(products){
+    localStorage.setItem("products",JSON.stringify(products));
+  }
 }
 
 document.addEventListener("DOMContentLoaded", ()=>{
   const ui = new UI();
   const products = new Products();
 
-  products.getProducts().then(products => ui.displayproducts(products));
+  products.getProducts().then(products =>{
+    ui.displayproducts(products);
+    Storage.saveProducts(products);
+
+  } );
+  
 });
