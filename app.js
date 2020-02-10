af60cdc186371d5514f530015a390192d6573526
Souravg87 <46392946+Souravg87@users.noreply.github.com>
2020-02-10 22:30:07 +0530

    adding items to cart

--- app.js
+++ app.js
@@ -87,7 +87,9 @@ class UI {
           // set cart values
           this.setCartValues(cart);
           // display cart item
+          this.addCartItem(cartItem)
           // show the cart
+          this.showCart();
 
         });
       
@@ -104,9 +106,32 @@ class UI {
     })
     cartTotal.innerText = parseFloat(tempTotal.toFixed(2));
     cartItems.innerText = itemsTotal;
-    console.log(cartTotal,cartItems);
+  }
+  addCartItem(item){
+    const div = document.createElement('div');
+    div.classList.add('cart-item');
+    div.innerHTML = `<img src=${item.image} alt="product">
+    <div>
+      <h4>${item.title}</h4>
+      <h5>$${item.price}</h5>
+      <span class="remove-item" data-id=${item.id}>remove</span>
+    </div>
+    <div>
+      <i class="fas fa-chevron-up" data-id=${item.id}></i>
+      <p class="item-amount">${item.amount}</p>
+      <i class="fas fa-chevron-down" data-id=${item.id}></i>
+    </div>`;
+    cartContent.appendChild(div);
+    
     
   }
+  showCart(){
+    cartOverlay.classList.add('transparentBcg');
+    cartDOM.classList.add('showCart');
+
+  }
+
+  
   
 }
 
